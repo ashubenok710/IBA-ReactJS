d1d99e255b6de016ec576ee5b4f42a09311f50cc
ashubenok710 <ashubenok710@gmail.com>
2020-06-11 14:02:27 +0300

    Task2 issues were fixed

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
-import React, { Component } from 'react';
-import { Row, Container } from 'react-bootstrap';
-import './App.css';
+import React from "react";
+import { Row, Container } from "react-bootstrap";
+import "./App.css";
 
-import Header from './Header'
-import Card from './Card'
-import NewCard from './NewCard'
-
-
+import Header from "./Header";
+import Card from "./Card";
 
 function App() {
-
-  function Checkbox() {
-    const [checked, setChecked] = React.useState(true);
-
-    return (
-      <label>
-        <input type="checkbox"
-          checked={checked}
-          onChange={() => setChecked(!checked)}
-        />
-      </label>
-    );
-  }
-
-
   return (
     <div className="App">
-
       <Container className="main-container">
         <Row>
-          <Header headerText="Header" > </Header>
+          <Header headerText="Header"> </Header>
         </Row>
         <Row className="cardWrapper">
-          <Card headerText="Caption" >Text...</Card>
-        </Row>
-
-        <Row className="cardWrapper">
-          <NewCard headerText="New Caption" >New Text...</NewCard>
+          <Card headerText="Caption">Text...</Card>
         </Row>
       </Container>
-
     </div>
   );
 }
--- src/Card/index.js
+++ src/Card/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import './Card.css';
+import React, { Component } from "react";
+import StatelessCheckbox from "../StatelessCheckbox/index.js";
+import "./Card.css";
+import classNames from "classnames/bind";
 
-const Card = (props) =>
-    <div className="card">
-        <div className="card-header">{props.headerText}</div>
-        <div>{props.children}</div>
-    </div>
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeMethod = this.changeMethod.bind(this);
+  }
+
+  state = {
+    checked: false,
+  };
+
+  changeMethod() {
+    this.setState({
+      checked: !this.state.checked,
+    });
+  }
+
+  render() {
+    const { checked } = this.state;
+    var cardClass = classNames("card", this.props.className, {
+      "card-checked": this.state.checked,
+    });
+
+    return (
+      <div className={cardClass}>
+        {/* alternate approach  </div><div className={`card ${checked ? "card-styled" : ""}`}>  */}
+        <span className="absolute-checkbox">
+          <StatelessCheckbox checked={checked} onChange={this.changeMethod} />
+        </span>
+        <div className="card-header">{this.props.headerText}</div>
+        <div>{this.props.children}</div>
+      </div>
+    );
+  }
+}
 
 export default Card;
