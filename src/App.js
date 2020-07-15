5fdee958b3125fc265e783f07e989c8bb8c68394
ashubenok710 <ashubenok710@gmail.com>
2020-07-15 15:47:17 +0300

    Task4 - added 7 cards

--- src/App.js
+++ src/App.js
@@ -23,17 +23,32 @@ class App extends React.Component {
   render() {
     const { header, text } = this.state;
 
+    const data = [
+      { id: 1, headerText: "Caption1", text: "Text1" },
+      { id: 2, headerText: "Caption2", text: "Text2" },
+      { id: 3, headerText: "Caption3", text: "Text3" },
+      { id: 4, headerText: "Caption4", text: "Text4" },
+      { id: 5, headerText: "Caption5", text: "Text5" },
+      { id: 6, headerText: "Caption6", text: "Text6" },
+      { id: 7, headerText: "Caption7", text: "Text7" },
+    ];
+    //const listItems = data.map((d) => <li key={d.name}>{d.name}</li>);
+
+    const listItems = data.map((d) => (
+      <Row className="cardWrapper">
+        <Card key={d.id} headerText={d.headerText} onSetCard={this.setCard}>
+          {d.text}
+        </Card>
+      </Row>
+    ));
+
     return (
       <div className="App">
         <Container className="main-container">
           <Row>
             <Header headerText="Header"></Header>
           </Row>
-          <Row className="cardWrapper">
-            <Card headerText={header} onSetCard={this.setCard}>
-              {text}
-            </Card>
-          </Row>
+          <Row className="cardWrapper">{listItems}</Row>
         </Container>
       </div>
     );
